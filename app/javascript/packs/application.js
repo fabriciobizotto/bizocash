7c5e7bc297e6939f3a7396b9281c345241d51955
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2021-06-02 10:07:46 -0300

    css tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "../styles/application.scss";
+require("styles/application.scss")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
